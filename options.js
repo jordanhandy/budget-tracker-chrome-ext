e4bd08b185ce0a7614cf057a471313e16554670f
Jordan Handy <handy.jordan@gmail.com>
2020-10-03 20:31:10 -0400

    add extension notifications

--- options.js
+++ options.js
@@ -1,3 +1,11 @@
+// This is for Options page (when you right-click on a given extension)
+// When the options page is entered, grab the Limit information that already
+// exists in Chrome storage (if there is one)
+
+// When a user enters a value in the input box and chooses to save the
+// limit, the chrome storage value is set to the new value the user
+// specified, and the page is closed
+
 $(function(){
 
     chrome.storage.sync.get('limit',function(budget){
@@ -11,7 +19,17 @@ $(function(){
             })
         }
     });
+    // The user is sent a notification that their spending total
+    // has been reset to zero
     $('#resetTotal').click(function(){
-        chrome.storage.sync.set({'total': 0});
+        chrome.storage.sync.set({'total': 0},function(){
+            var notifOptions = {
+                type: 'basic',
+                iconURL: 'icon48.png',
+                title: 'Total Reset!',
+                message: "Your spending total has been reset to zero (0)"
+            };
+            chrome.notifications.create('limitNotif',notifOptions);
+        });
     });
 });
--- popup.js
+++ popup.js
@@ -20,8 +20,10 @@ $(function(){
         $('#limit').text(budget.limit);
     })
 
+    // on click, the current total and limit values are populated,
+    // if they exist
     $('#spendAmount').click(function(){
-        chrome.storage.sync.get('total',function(budget){
+        chrome.storage.sync.get(['total','limit'],function(budget){
             var newTotal = 0;
             if(budget.total){
                 newTotal += parseInt(budget.total);
@@ -30,7 +32,21 @@ $(function(){
             if(amount){
                 newTotal += parseInt(amount);
             }
-            chrome.storage.sync.set({'total': newTotal});
+            // The user is sent a notification that they have reached their
+            // desired spending limit
+            chrome.storage.sync.set({'total': newTotal},function(){
+                if(amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type: 'basic',
+                        iconURL: 'icon48.png',
+                        title: 'Limit reached!',
+                        message: "Uh-oh.  Looks like you've reached your spending limit!"
+                    };
+                    chrome.notifications.create('limitNotif',notifOptions);
+                }
+            });
+            // update the current spent total,
+            // and clear the input box of the user's reported value
             $('#total').text(newTotal);
             $('#amount').val('');
         });
