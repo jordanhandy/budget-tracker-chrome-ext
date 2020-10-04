c81da9ae198c884d6d6338055bac10b04b4e98ef
Jordan Handy <handy.jordan@gmail.com>
2020-10-03 23:08:57 -0400

    add context menu function to add integers to extension total

--- eventPage.js
+++ eventPage.js
@@ -7,4 +7,45 @@ var contextMenuItem ={
     "title": "SpendMoney",
     "contexts": ["selection"]
 };
-chrome.contextMenus.create(contextMenuItem)
+chrome.contextMenus.create(contextMenuItem);
+
+function isInt(value){
+    return !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value,10));
+}
+
+// listen for the click event on the context menu
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    // if the user clicked on the spendMoney item, and they have
+    // text selected
+    if(clickData.menuItemId == "spendMoney" && clickData.selectionText)
+    {
+        // if the selected data is an integer
+        if(isInt(clickData.selectionText))
+        {
+            // get the total spend and the spend limit
+            // if a total already exists, add already existed total
+            // to the total spend to be added.  If there is not a
+            // currently existing toal, then add to the total using the
+            // selected text
+            chrome.storage.sync.get(['total','limit'], function(budget){
+                var newTotal = 0;
+                if (budget.total){
+                    newTotal += parseInt(budget.total);
+                }
+                newTotal += parseInt(clickData.selectionText);
+                chrome.storage.sync.set({'total': newTotal},function(){
+                    if (newTotal >= budget.limit)
+                    {
+                        var notifOptions = {
+                            type: 'basic',
+                            iconURL: 'icon48.png',
+                            title: 'Limit reached!',
+                            message: "Uh-oh.  Looks like you've reached your spending limit!"
+                        };
+                        chrome.notifications.create('limitNotif',notifOptions);
+                    }
+                });
+            });
+        }
+    }
+});
